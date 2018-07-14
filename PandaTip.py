04ef7ab2091c448876a76fce83449bd47f3dfc3d
Jahus <jahus@live.ie>
2018-07-14 22:17:39 +0100

    Version 1.1 - Some PEP8; - Cleaner script running (uses "__main__" instead of bare script); - Simplified the use of CommandHandler() and add_handler(); - WIP toward using UserID instead of username; - Added some ToDo.

--- PandaTip.py
+++ PandaTip.py
@@ -1,5 +1,6 @@
 #-*- coding: utf-8 -*-
 
+
 import json
 import codecs
 import requests
@@ -9,36 +10,67 @@ import subprocess
 from telegram.ext.dispatcher import run_async
 from telegram.ext import Updater
 from html import escape
+from telegram.ext import CommandHandler
+from telegram import ParseMode
 
-updater = Updater(token='BOT_TOKEN')
-dispatcher = updater.dispatcher
+
+# Constants
+__wallet = "/usr/local/bin/pandacoind"
+
 
 import logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
 					level=logging.INFO)
 
+
 def commands(bot, update):
 	user = update.message.from_user.username 
-	bot.send_message(chat_id=update.message.chat_id, text="Initiating commands /tip & /withdraw have a specfic format,\n use them like so:" + "\n \n Parameters: \n <user> = target user to tip \n <amount> = amount of pandacoin to utilise \n <address> = pandacoin address to withdraw to \n \n Tipping format: \n /tip <user> <amount> \n \n Withdrawing format: \n /withdraw <address> <amount>")
+	bot.send_message(
+		chat_id=update.message.chat_id,
+		text="Initiating commands /tip & /withdraw have a specfic format,\n"
+			"use them like so:\n \n Parameters: \n <user> = target user to tip \n"
+			" <amount> = amount of pandacoin to utilise \n"
+			" <address> = pandacoin address to withdraw to"
+			" \n \n Tipping format: \n"
+			" /tip <user> <amount> \n \n Withdrawing format: \n"
+			" /withdraw <address> <amount>"
+	)
+
 
 def help(bot, update):
-	bot.send_message(chat_id=update.message.chat_id, text="The following commands are at your disposal: /hi , /commands , /deposit , /tip , /withdraw , /price , /marketcap or /balance")
+	bot.send_message(
+		chat_id=update.message.chat_id,
+		text="The following commands are at your disposal: /hi , /commands , /deposit , /tip , /withdraw , /price , /marketcap or /balance"
+	)
+
 
 def deposit(bot, update):
-	user = update.message.from_user.username
-	if user is None:
-		bot.send_message(chat_id=update.message.chat_id, text="Please set a telegram username in your profile settings!")
+	chat_id = str(update.effective_chat.id)
+	if update.effective_chat is None:
+		_chat_type = "private"
+	elif update.effective_chat.type == "private":
+		_chat_type = "private"
 	else:
-		address = "/usr/local/bin/pandacoind"
-		result = subprocess.run([address,"getaccountaddress",user],stdout=subprocess.PIPE)
-		clean = (result.stdout.strip()).decode("utf-8")
-		bot.send_message(chat_id=update.message.chat_id, text="@{0} your depositing address is: {1}".format(user,clean))
+		_chat_type = "group"
+	# Only show deposit address if it's a private conversation with the bot
+	if _chat_type == "private":
+		_result = subprocess.run([__wallet, "getaccountaddress", chat_id], stdout=subprocess.PIPE)
+		_address = (_result.stdout.strip()).decode("utf-8")
+		update.message.reply_text(
+			text="Your depositing address is: `%s`" % _address,
+			quote=True,
+			parse_mode=ParseMode.MARKDOWN,
+			disable_web_page_preview=True
+		)
 
+
+# ToDo: Rewrite the whole logic; use tags instead of parsing usernames
+# ToDo: Allow private tipping if the user can be tagged (@username available)
 def tip(bot,update):
 	user = update.message.from_user.username
 	target = update.message.text[5:]
-	amount =  target.split(" ")[1]
-	target =  target.split(" ")[0]
+	amount = target.split(" ")[1]
+	target = target.split(" ")[0]
 	if user is None:
 		bot.send_message(chat_id=update.message.chat_id, text="Please set a telegram username in your profile settings!")
 	else:
@@ -64,6 +96,9 @@ def tip(bot,update):
 		else: 
 			bot.send_message(chat_id=update.message.chat_id, text="Error that user is not applicable.")
 
+
+# ToDo: Give balance only if a private chat
+# ToDo: Remove WorldCoinIndex
 def balance(bot,update):
 	quote_page = requests.get('https://www.worldcoinindex.com/coin/pandacoin')
 	strainer = SoupStrainer('div', attrs={'class': 'row mob-coin-table'})
@@ -86,6 +121,7 @@ def balance(bot,update):
 		balance =  str(round(balance,3))
 		bot.send_message(chat_id=update.message.chat_id, text="@{0} your current balance is: {1} PND ≈  ${2}".format(user,balance,fiat_balance))
 
+
 def price(bot,update):
 	quote_page = requests.get('https://www.worldcoinindex.com/coin/pandacoin')
 	strainer = SoupStrainer('div', attrs={'class': 'row mob-coin-table'})
@@ -101,6 +137,7 @@ def price(bot,update):
 	btc = soup[80:]
 	sats = btc[:-2]
 	bot.send_message(chat_id=update.message.chat_id, text="Pandacoin is valued at {0} Δ {1} ≈ {2}".format(price,percent,sats) + " ฿")
+
 
 def withdraw(bot,update):
 	user = update.message.from_user.username
@@ -123,12 +160,15 @@ def withdraw(bot,update):
 			tx = subprocess.run([core,"sendfrom",user,address,amount],stdout=subprocess.PIPE)
 			bot.send_message(chat_id=update.message.chat_id, text="@{0} has successfully withdrew to address: {1} of {2} PND" .format(user,address,amount))
 
+
 def hi(bot,update):
 	user = update.message.from_user.username
 	bot.send_message(chat_id=update.message.chat_id, text="Hello @{0}, how are you doing today?".format(user))
 
+
 def moon(bot,update):
-  bot.send_message(chat_id=update.message.chat_id, text="Moon mission inbound!")
+	bot.send_message(chat_id=update.message.chat_id, text="Moon mission inbound!")
+
 
 def marketcap(bot,update):
 	quote_page = requests.get('https://www.worldcoinindex.com/coin/pandacoin')
@@ -139,37 +179,18 @@ def marketcap(bot,update):
 	mc = re.sub(r'\n\s*\n', r'\n\n', name.strip(), flags=re.M)
 	bot.send_message(chat_id=update.message.chat_id, text="The current market cap of Pandacoin is valued at {0}".format(mc))
 
-from telegram.ext import CommandHandler
 
-commands_handler = CommandHandler('commands', commands)
-dispatcher.add_handler(commands_handler)
-
-moon_handler = CommandHandler('moon', moon)
-dispatcher.add_handler(moon_handler)
-
-hi_handler = CommandHandler('hi', hi)
-dispatcher.add_handler(hi_handler)
-
-withdraw_handler = CommandHandler('withdraw', withdraw)
-dispatcher.add_handler(withdraw_handler)
-
-marketcap_handler = CommandHandler('marketcap', marketcap)
-dispatcher.add_handler(marketcap_handler)
-
-deposit_handler = CommandHandler('deposit', deposit)
-dispatcher.add_handler(deposit_handler)
-
-price_handler = CommandHandler('price', price)
-dispatcher.add_handler(price_handler)
-
-tip_handler = CommandHandler('tip', tip)
-dispatcher.add_handler(tip_handler)
-
-balance_handler = CommandHandler('balance', balance)
-dispatcher.add_handler(balance_handler)
-
-help_handler = CommandHandler('help', help)
-dispatcher.add_handler(help_handler)
-
-updater.start_polling()
-
+if __name__ == "__main__":
+	updater = Updater(token='BOT_TOKEN')
+	dispatcher = updater.dispatcher
+	dispatcher.add_handler(CommandHandler('commands', commands))
+	dispatcher.add_handler(CommandHandler('moon', moon))
+	dispatcher.add_handler(CommandHandler('hi', hi))
+	dispatcher.add_handler(CommandHandler('withdraw', withdraw))
+	dispatcher.add_handler(CommandHandler('marketcap', marketcap))
+	dispatcher.add_handler(CommandHandler('deposit', deposit))
+	dispatcher.add_handler(CommandHandler('price', price))
+	dispatcher.add_handler(CommandHandler('tip', tip))
+	dispatcher.add_handler(CommandHandler('balance', balance))
+	dispatcher.add_handler(CommandHandler('help', help))
+	updater.start_polling()
